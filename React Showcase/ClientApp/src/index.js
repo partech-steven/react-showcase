daf356e8a8fa17a6cfdc9c583c03e4cc7fbfa85b
Steven Nolles <steven@partech365.nl>
2021-09-13 16:03:35 +0200

    ASP Migration success!

--- React Showcase/ClientApp/src/index.js
+++ React Showcase/ClientApp/src/index.js
@@ -5,11 +5,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+//Style
+import './style.css';
+
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
-
-//Style
-import './style.scss';
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
