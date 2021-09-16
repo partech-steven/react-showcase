af7c5eae2f7c3656a28a937c20627faace63b98f
Steven Nolles <steven@partech365.nl>
2021-09-16 11:04:50 +0200

    The homepage now has proper text

--- React Showcase/ClientApp/src/js/components/Pages/Home.js
+++ React Showcase/ClientApp/src/js/components/Pages/Home.js
@@ -4,7 +4,19 @@ export class Home extends Component {
     render() {
         return (
             <div className="content">
-                Ok doei
+                <h1>Welcome!</h1>
+                <p>
+                    Welcome to this humble little website where I, Steven Nolles, show off some of my projects in different showcases.
+                    <br/> Use the menu above to navigate through the website.
+                </p>
+                <p>
+                    Please note that whilst this website was made using bootstrap it has not been optimized for mobile use. This is due to the fact that one of my projects, Twitter API Integration, contains drag-and-drop functionalities which can be slightly bothersome on mobile.
+                    <br/> Should you view this website on a mobile device or tablet, don't be alarmed if things look slightly... out of place.
+                </p>
+                <p>
+                    Am I planning to ever rectify this? Yes.
+                    <br />Do I know when? Soon&#8482;
+                </p>
             </div>
         );
     }
